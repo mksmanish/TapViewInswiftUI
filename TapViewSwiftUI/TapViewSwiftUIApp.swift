c4bfd451727c48aa6b0ac6f0b6ec778effd68dd6
manish <manishk@winjit.com>
2021-06-22 18:34:32 +0530

    Initial Commit

---
+++ TapViewSwiftUI/TapViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapViewSwiftUIApp.swift
+//  TapViewSwiftUI
+//
+//  Created by manish on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TapViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
